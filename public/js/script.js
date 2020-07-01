6ad92c7b18515473518e2e3aa3123853a6c980fb
Gaurav <gauravsharma10167@gmail.com>
2020-07-01 11:25:52 +0530

    speed of mouse

--- public/js/script.js
+++ public/js/script.js
@@ -4,23 +4,40 @@ var init_x = 0,
     init_x_mobile = 0,
     init_y_mobile = 0,
     final_x = 0,
-    final_y = 0;
+    final_y = 0,
+    start_time,
+    max_distance_x = 0,
+    max_distance_y = 0;
 
 function onStart(e) {
     e.preventDefault();
     init_x_mobile = e.touches[0].clientX;
     init_y_mobile = e.touches[0].clientY;
+    start_time = new Date().getTime();
 }
 
 function onMove(e) {
-    var curr_x = e.touches[0].clientX;
-    var curr_y = e.touches[0].clientY;
+    var curr_x_mobile = e.touches[0].clientX;
+    var curr_y_mobile = e.touches[0].clientY;
+    var curr_time = new Date().getTime();
 
-    var distance_x = curr_x - init_x_mobile;
-    var distance_y = curr_y - init_y_mobile;
+    var elapsed_time = curr_time - start_time;
+    var distance_x_mobile = curr_x_mobile - init_x_mobile;
+    var distance_y_mobile = curr_y_mobile - init_y_mobile;
+    if (Math.abs(distance_x_mobile) >= max_distance_x &&
+        Math.abs(distance_y_mobile) >= max_distance_y) {
+        max_distance_x = Math.abs(distance_x_mobile);
+        max_distance_y = Math.abs(distance_y_mobile);
+    } else {
+        onStop(e);
+        onStart(e);
+    }
 
-    final_x = init_x + distance_x;
-    final_y = init_y + distance_y;
+    var speed_x = Math.max(1.5, Math.abs(distance_x_mobile) / elapsed_time);
+    var speed_y = Math.max(1.5, Math.abs(distance_y_mobile) / elapsed_time);
+
+    final_x = init_x + distance_x_mobile * speed_x;
+    final_y = init_y + distance_y_mobile * speed_y;
     socket.emit("move", { final_x, final_y });
 }
 
